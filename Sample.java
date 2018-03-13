4d2ad39a6503a4fe8e0f48acf5e4e4f03705e4ee
ccowperthwait <ccowperthwait@gmail.com>
2018-03-13 15:42:52 -0600

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,13 @@
+public class Sample {
+	public static void main(String[] args) {
+		if (args == null) {
+			System.out.println("args==null");
+		} else if (args.length == 0) {
+			System.out.println("args.length==0");
+		} else {
+			for (int i = 0; i < args.length; i++) {
+				System.out.println("args[" + i + "]=[" + args[i] + "]");
+			}
+		}
+	}
+}
